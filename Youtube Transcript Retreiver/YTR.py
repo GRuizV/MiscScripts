ba79686516776666dc56aa6484f98d3e50a94d8a
Gerardo Ruiz <grville93@gmail.com>
2024-07-08 16:05:37 -0500

    YTR script modified to work independent where is executed.

--- Youtube Transcript Retreiver/YTR.py
+++ Youtube Transcript Retreiver/YTR.py
@@ -1,18 +1,43 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+import os
 
-JSON_PATH = r'C:\Users\USUARIO\GR\Software Development\Misc Scripts\Youtube Transcript Retreiver\Result.txt'
+# Obtain the directory where the script is
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, 'Result.txt')
 
-video_id = 'JSehk8lKkYs'
+# Youtube Vid parameter
+video_id = 'KRjyYh9Fd5w'
+
+# String holder
 transcript_string = ''
 
+# Get the video transcript
 try:
     transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['es'])
-except:
-    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['es', 'en'])
+    # print("Transcripción obtenida en español.")
 
+except Exception as e:
+    # print(f"Error al obtener la transcripción en español: {e}")
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['es', 'en'])
+        # print("Transcripción obtenida en inglés.")
+
+    except Exception as e:
+        print(f"Error al obtener la transcripción en inglés: {e}")
+        transcript = []
+
+
+# Populate the string holder with the actual video's transcript
 for entry in transcript:
-   transcript_string += f"{entry['start']:.0f}s: {entry['text']}\n"
+    transcript_string += f"{entry['start']:.0f}s: {entry['text']}\n"
 
 
-with open(JSON_PATH, 'w') as file:
-    file.write(transcript_string)
+# Save the file
+try:    
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(transcript_string)
+
+    print(f"Transcripción guardada en {file_path}")
+
+except Exception as e:
+    print(f"Error al escribir en el archivo: {e}")
